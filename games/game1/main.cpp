adb4f04f91fabc6c76763ebe7fab6802519115a7
Uğur Güney <ugurguney@gmail.com>
2022-09-27 23:28:51 -0400

    Introduce State, PlayingState. All initialization and running logic and states are in Game class;

--- games/game1/main.cpp
+++ games/game1/main.cpp
@@ -109,13 +109,16 @@ public:
 	}
 };
 
-class Game {
-	enum class State {
-		Menu = 0, Playing, Pause, GameOver, Win
-	};
+class State {
+public:
+	virtual void handleEvent(const SDL_Event& e) = 0;
+	virtual State* update(uint32_t deltaTime) = 0;
+	virtual void render(SDL_Renderer* gRenderer, TTF_Font* gFont) = 0;
+	virtual ~State() = default;
+};
 
+class PlayingState : public State {
 private:
-	State state = State::Menu;
 	int32_t gridSize{ 15 };
 	SDL_Keycode lastKey;
 	Snake snake;
@@ -123,17 +126,18 @@ private:
 	uint32_t score{};
 	uint32_t timer{};
 	std::mt19937 rnd = std::mt19937{ std::random_device{}() };
+	//State* state;
 public:
 	int period = 200;
 
 public:
-	Game() : lastKey{ SDLK_UNKNOWN }, snake{ Cell{gridSize / 2, gridSize / 2}, 2, Direction::LEFT } {
+	PlayingState() : lastKey{ SDLK_UNKNOWN }, snake{ Cell{gridSize / 2, gridSize / 2}, 2, Direction::LEFT } {
 		for (const Cell& cell : snake.getCells())
 			assert(!cell.isAtGridWalls(gridSize));
 		placeApple();
 	}
 
-	void handleEvent(const SDL_Event& e) {
+	void handleEvent(const SDL_Event& e)  final{
 		if (e.type == SDL_KEYDOWN && e.key.repeat == 0)
 			lastKey = e.key.keysym.sym;
 	}
@@ -155,7 +159,7 @@ public:
 		apple = out[0];
 	}
 
-	void render(SDL_Renderer* gRenderer, TTF_Font* gFont) {
+	void render(SDL_Renderer* gRenderer, TTF_Font* gFont) final {
 		// Clear
 		SDL_SetRenderDrawColor(gRenderer, 0x88, 0x88, 0x88, 0xFF);
 		SDL_RenderClear(gRenderer);
@@ -194,10 +198,10 @@ public:
 		}
 	}
 
-	void update(uint32_t deltaTime) {
+	State* update(uint32_t deltaTime) final {
 		timer += deltaTime;
 		if (timer < period)
-			return;
+			return this;
 		timer -= period;
 
 		switch (lastKey) {
@@ -224,51 +228,64 @@ public:
 		}
 		else
 			snake.move();
+
+		return this;
+	}
+};
+
+class Game {
+public:
+	PlayingState playingState;
+	State* state;
+public:
+	void run() {
+		SDL_Init(SDL_INIT_VIDEO);
+		TTF_Init();
+		SDL_Window* gWindow = SDL_CreateWindow("Snake", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SIZE, SIZE, SDL_WINDOW_SHOWN);
+		//SDL_Surface* gScreenSurface = SDL_GetWindowSurface(gWindow);
+		SDL_Renderer* gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+		const char* fontFile = "assets/fonts/enter_command/EnterCommand.ttf"; // "c:\\Windows\\Fonts\\vgaoem.fon"; // arial.ttf"
+		TTF_Font* gFont = TTF_OpenFont(fontFile, 28);
+		if (gFont == nullptr)
+			std::cout << "font not found! " << TTF_GetError() << "\n";
+		TTF_SetFontStyle(gFont, TTF_STYLE_NORMAL);
+
+		SDL_UpdateWindowSurface(gWindow);
+		SDL_Event e;
+		bool quit = false;
+
+		state = &playingState;
+		uint32_t time = SDL_GetTicks();
+		while (!quit) {
+			while (SDL_PollEvent(&e)) {
+				if (e.type == SDL_QUIT)
+					quit = true;
+				else
+					state->handleEvent(e);
+			}
+
+			uint32_t deltaTime = SDL_GetTicks() - time;
+			time = SDL_GetTicks();
+			state = state->update(deltaTime);
+
+			SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0xFF, 0xFF);
+			SDL_RenderClear(gRenderer);
+
+			state->render(gRenderer, gFont);
+
+			SDL_RenderPresent(gRenderer);
+		}
+
+		TTF_CloseFont(gFont);
+		TTF_Quit();
+		SDL_DestroyRenderer(gRenderer);
+		SDL_DestroyWindow(gWindow);
+		SDL_Quit();
 	}
 };
 
 int main(int argc, char* args[]) {
-	SDL_Init(SDL_INIT_VIDEO);
-	TTF_Init();
-	SDL_Window* gWindow = SDL_CreateWindow("Snake", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SIZE, SIZE, SDL_WINDOW_SHOWN);
-	//SDL_Surface* gScreenSurface = SDL_GetWindowSurface(gWindow);
-	SDL_Renderer* gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
-	const char* fontFile = "assets/fonts/enter_command/EnterCommand.ttf"; // "c:\\Windows\\Fonts\\vgaoem.fon"; // arial.ttf"
-	TTF_Font* gFont = TTF_OpenFont(fontFile, 28);
-	if (gFont == nullptr)
-		std::cout << "font not found! " << TTF_GetError() << "\n";
-	TTF_SetFontStyle(gFont, TTF_STYLE_NORMAL);
-
-	SDL_UpdateWindowSurface(gWindow);
-	SDL_Event e;
-	bool quit = false;
 	Game game;
-
-	uint32_t time = SDL_GetTicks();
-	while (!quit) {
-		while (SDL_PollEvent(&e)) {
-			if (e.type == SDL_QUIT)
-				quit = true;
-			else
-				game.handleEvent(e);
-		}
-
-		uint32_t deltaTime = SDL_GetTicks() - time;
-		time = SDL_GetTicks();
-		game.update(deltaTime);
-
-		SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0xFF, 0xFF);
-		SDL_RenderClear(gRenderer);
-
-		game.render(gRenderer, gFont);
-
-		SDL_RenderPresent(gRenderer);
-	}
-
-	TTF_CloseFont(gFont);
-	TTF_Quit();
-	SDL_DestroyRenderer(gRenderer);
-	SDL_DestroyWindow(gWindow);
-	SDL_Quit();
+	game.run();
 	return 0;
 }
