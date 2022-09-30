d82715bff6251fc707584f17ba2b8c524b2065d5
Uğur Güney <ugurguney@gmail.com>
2022-09-30 09:26:40 -0400

    gds game-dev lib abstracting SDL.
    Currently only function is renderText more will come

---
+++ games/lib/gds.cpp
@@ -0,0 +1,18 @@
+#include "gds.h"
+
+#include <iostream>
+
+void renderText(const std::string& text, SDL_Color color, int x, int y, SDL_Renderer* renderer, TTF_Font* font, bool center) {
+	SDL_Surface* textSurface = TTF_RenderText_Blended(font, text.c_str(), color);   // anti-aliased glyphs, TTF_RenderText_Solid() for aliased glyphs
+	if (textSurface == nullptr)
+		std::cerr << "Unable to render text surface! SDL_ttf Error: " << TTF_GetError() << "\n";
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+	int texW, texH;
+	SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
+	SDL_Rect dstrect = center ? SDL_Rect{ x - texW / 2, y - texH / 2, texW, texH } : SDL_Rect{ x, y, texW, texH };
+	SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+
+	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(textSurface);
+}
---
+++ games/lib/gds.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+#include <string>
+
+void renderText(const std::string& text, SDL_Color color, int x, int y, SDL_Renderer* renderer, TTF_Font* font, bool center = false);
